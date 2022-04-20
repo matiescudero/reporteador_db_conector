fb61526e545d94043fce6a5dbc4cbaa33ef31203
matiescudero <matias.escudero.b@usach.cl>
2022-04-20 16:50:18 -0400

    cambios sql

--- sql_queries/add_geometry_centros.sql
+++ sql_queries/add_geometry_centros.sql
@@ -7,12 +7,12 @@
 ALTER TABLE entradas.concesiones_acuicultura
 ADD COLUMN geom geometry(Geometry, 102100);
 
--- Se pobla la nueva columna con la geometría basada en el campo 'geometry'
+-- Se pobla la nueva columna con la geometria basada en el campo 'geometry'
 
 UPDATE entradas.concesiones_acuicultura
 SET geom = ST_PolygonFromText(geometry, 102100);
 
--- Se elimina la columna que contiene la geometría como texto
+-- Se elimina la columna que contiene la geometria como texto
 
 ALTER TABLE entradas.concesiones_acuicultura
 DROP COLUMN geometry;
