13ba420811eb42069028021016da1bb2816d5942
matiescudero <matias.escudero.b@usach.cl>
2022-05-10 16:57:56 -0400

    procesamiento de geomtrias nuevas

--- sql_queries/add_geometry_centros.sql
+++ sql_queries/add_geometry_centros.sql
@@ -1,6 +1,8 @@
 -----------------------------------------------------------------------
--- /* Se crea el campo geom para los centros provenientes del IDE */ --
+-- /* Se crea el campo geom para las tablas provenientes del IDE */ --
 -----------------------------------------------------------------------
+
+/* 1. Centros de Acuicultura */
 
 -- Se crea una nueva columna
 
@@ -16,4 +18,73 @@ SET geom = ST_PolygonFromText(geometry, 4326);
 
 ALTER TABLE entradas.concesiones_acuicultura
 DROP COLUMN geometry;
+
+/* 2. Areas de Colecta */
+
+-- Se crea una nueva columna
+
+ALTER TABLE entradas.areas_colecta
+ADD COLUMN geom geometry(Geometry, 4326);
+
+-- Se pobla la nueva columna con la geometria basada en el campo 'geometry'
+
+UPDATE entradas.areas_colecta
+SET geom = ST_PolygonFromText(geometry, 4326);
+
+-- Se elimina la columna que contiene la geometria como texto
+
+ALTER TABLE entradas.areas_colecta
+DROP COLUMN geometry;
+
+/* 3. ECMPO */
+
+-- Se crea una nueva columna
+
+ALTER TABLE entradas.ecmpo
+ADD COLUMN geom geometry(Geometry, 4326);
+
+-- Se pobla la nueva columna con la geometria basada en el campo 'geometry'
+
+UPDATE entradas.ecmpo
+SET geom = ST_PolygonFromText(geometry, 4326);
+
+-- Se elimina la columna que contiene la geometria como texto
+
+ALTER TABLE entradas.ecmpo
+DROP COLUMN geometry;
+
+/* 4. AMERB */
+
+-- Se crea una nueva columna
+
+ALTER TABLE entradas.amerb
+ADD COLUMN geom geometry(Geometry, 4326);
+
+-- Se pobla la nueva columna con la geometria basada en el campo 'geometry'
+
+UPDATE entradas.amerb
+SET geom = ST_PolygonFromText(geometry, 4326);
+
+-- Se elimina la columna que contiene la geometria como texto
+
+ALTER TABLE entradas.amerb
+DROP COLUMN geometry;
+
+/* 5. Acuicultura en AMERB */
+
+-- Se crea una nueva columna
+
+ALTER TABLE entradas.acuiamerb
+ADD COLUMN geom geometry(Geometry, 4326);
+
+-- Se pobla la nueva columna con la geometria basada en el campo 'geometry'
+
+UPDATE entradas.acuiamerb
+SET geom = ST_PolygonFromText(geometry, 4326);
+
+-- Se elimina la columna que contiene la geometria como texto
+
+ALTER TABLE entradas.acuiamerb
+DROP COLUMN geometry;
+
 
